c9abe7ee09afd492cd11ca8a87e1ad8b46f8bb17
giovanny2 <agiovannylutin@gmail.com>
2021-08-28 22:58:37 -0600

    falta que mande la opcion correcta

--- src/vistas/secundario1_Usuario.vue
+++ src/vistas/secundario1_Usuario.vue
@@ -1,155 +1,190 @@
 <template>
-    <div class="datospersonales">
-        <form>
-         <div v-show="vista==='noanonimo'">
-            <div class="Titulo">
-            <h4><span class="indicador">1</span> Datos Personales</h4>
-        </div>
-        <div class="formulario1" >
-             
+<div class="datospersonales">
+    <form>
+        <input id="prodId" name="prodId" type="text" style="" v-model="form.opcion2" value="1">
+        <div v-show="vista==='noanonimo'">
+                <div class="Titulo">
+                <h4><span class="indicador">1</span> Datos Personales</h4>
+            </div>
+
+            <div class="formulario1" >
                 <div class="form-row">
                     <div class="col">
                         <label for="exampleInputEmail1">Nombre</label>
-                        <input type="text" class="form-control" id="nombre" placeholder="Nombre completo" required>
+                        <input type="text" v-model="form.nombre" class="form-control" id="nombre" placeholder="Nombre completo">
                     </div>
                     <div class="col">
                         <label for="exampleInputEmail1">No. CUI</label>
-                        <input type="text" class="form-control" id="cui" placeholder="CUI" required>
+                        <input type="text" v-model="form.cui" class="form-control" id="cui" placeholder="CUI" >
                     </div>
                 </div>
-
                 <div class="form-row">
                     <div class="col">
                         <label for="exampleInputEmail1">Telefono</label>
-                        <input type="text" class="form-control" id="telefono" placeholder="Telefono">
+                        <input type="text" v-model="form.telefono" class="form-control" id="telefono" placeholder="Telefono">
                     </div>
                     <div class="col">
-                        <label for="exampleInputEmail1">Celular</label>
-                        <input type="text" class="form-control" id="celular" placeholder="Celular" required>
+                            <label for="exampleInputEmail1">Celular</label>
+                            <input type="text" v-model="form.celular" class="form-control" id="celular" placeholder="Celular" >
                     </div>
                     <div class="col">
-                        <label for="exampleInputEmail1">Correo</label>
-                        <input type="email" class="form-control" id="correo" placeholder="Correo">
+                            <label for="exampleInputEmail1">Correo</label>
+                            <input type="email" v-model="form.correo" class="form-control" id="correo" placeholder="Correo">
                     </div>
                 </div>
-         
-        </div>
+            </div>
         </div>   
         <br>
 
         <div class="Titulo">
             <h4><span class="indicador" v-if="vista==='noanonimo'">2</span>
             <span class="indicador" v-else>1</span>
-             Datos Proveedor</h4>
+            Datos Proveedor</h4>
         </div>
         <div class="formulario1">
-             <div class="form-row">
-                    <div class="col">
-                        <label for="exampleInputEmail1">Empresa</label>
-                        <input type="text" class="form-control" id="empresa" placeholder="Nombre de la empresa" >
-                    </div>
-                    <div class="col">
-                        <label for="exampleInputEmail1">Razon Social</label>
-                        <input type="text" class="form-control" id="razon" placeholder="Razon Social Empresa" >
-                    </div>
-                    <div class="col">
-                        <label for="exampleInputEmail1">Nit</label>
-                        <input type="text" class="form-control" id="nit" placeholder="Nit de la Empresa" >
-                    </div>
-             </div>
+            
+            <div class="form-row">
+                <div class="col">
+                    <label for="exampleInputEmail1">Empresa</label>
+                    <input type="text" v-model="form.empresa" class="form-control" id="empresa" placeholder="Nombre de la empresa" >
+                </div>
+                <div class="col">
+                    <label for="exampleInputEmail1">Razon Social</label>
+                    <input type="text" v-model="form.razon" class="form-control" id="razon" placeholder="Razon Social Empresa" >
+                </div>
+                <div class="col">
+                    <label for="exampleInputEmail1">Nit</label>
+                    <input type="text" v-model="form.nit" class="form-control" id="nit" placeholder="Nit de la Empresa" >
+                </div>
+            </div>
 
-              <div class="form-row">
-                    <div class="col">
-                        <label for="exampleInputEmail1">Direccion</label>
-                        <input type="text" class="form-control" id="direccion" placeholder="Direccion de la empresa" >
-                    </div>
-                    <div class="col">
-                        <label for="exampleInputEmail1">Zona</label>
-                        <input type="text" class="form-control" id="zona" placeholder="Zona Social Empresa" >
-                    </div>
-                    <div class="col">
-                        <label for="exampleInputEmail1">Telefono</label>
-                        <input type="text" class="form-control" id="telefonoe" placeholder="Telefono de la Empresa" >
-                    </div>
-             </div>
-                <div class="form-row">
-                    <div class="col">
-                        <label for="exampleInputEmail1">Departamento</label>
-                        <input type="text" class="form-control" id="departamento" placeholder="Departamento de la empresa" >
-                    </div>
-                    <div class="col">
-                        <label for="exampleInputEmail1">Municipio</label>
-                        <input type="text" class="form-control" id="municipio" placeholder="Municipio Social Empresa" >
-                    </div>
-             </div>
+            <div class="form-row">
+                <div class="col">
+                    <label for="exampleInputEmail1">Direccion</label>
+                    <input type="text" v-model="form.direccionemp" class="form-control" id="direccion" placeholder="Direccion de la empresa" >
+                </div>
+                <div class="col">
+                    <label for="exampleInputEmail1">Zona</label>
+                    <input type="text" v-model="form.zona" class="form-control" id="zona" placeholder="Zona Social Empresa" >
+                </div>
+                <div class="col">
+                    <label for="exampleInputEmail1">Telefono</label>
+                    <input type="text" v-model="form.telefonoemp" class="form-control" id="telefonoe" placeholder="Telefono de la Empresa" >
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col">
+                    <label for="exampleInputEmail1">Departamento</label>
+                    <input type="text" v-model="form.departamento" class="form-control" id="departamento" placeholder="Departamento de la empresa" >
+                </div>
+                <div class="col">
+                    <label for="exampleInputEmail1">Municipio</label>
+                    <input type="text" v-model="form.municipio" class="form-control" id="municipio" placeholder="Municipio Social Empresa" >
+                </div>
+            </div>
         </div>
         <br>
 
-         <div class="Titulo">
-            <h4><span class="indicador" v-if="vista==='noanonimo'">3</span>
-            <span class="indicador" v-else>2</span>
-             Detalle Queja</h4>
-        </div>
-         <div class="formulario1">
-                <div class="form-row">
-                    <div class="col">
-                        <label for="exampleInputEmail1">No. Factura</label>
-                        <input type="text" class="form-control" id="factura" placeholder="No. Factura" >
-                    </div>
-                    <div class="col">
-                        <label for="exampleInputEmail1">Fecha Emision</label>
-                        <input type="date" class="form-control" id="fecha" placeholder="Fecha Emision" >
-                    </div>
+            <div class="Titulo">
+                <h4><span class="indicador" v-if="vista==='noanonimo'">3</span>
+                <span class="indicador" v-else>2</span>
+                Detalle Queja</h4>
+            </div>
+            <div class="formulario1">
+                    <div class="form-row">
+                        <div class="col">
+                            <label for="exampleInputEmail1">No. Factura</label>
+                            <input type="text" class="form-control" id="factura" placeholder="No. Factura" >
+                        </div>
+                        <div class="col">
+                            <label for="exampleInputEmail1">Fecha Emision</label>
+                            <input type="date" class="form-control" id="fecha" placeholder="Fecha Emision" >
+                        </div>
 
-                </div>
-                  <div class="form-row">
-                    <div class="col">
-                        <label for="exampleInputEmail1">Detalle de lo sucedido</label>
-                        <textarea type="text" class="form-control" id="detalle1" placeholder="" ></textarea>
                     </div>
-                    <div class="col">
-                        <label for="exampleInputEmail1">Que solicita </label>
-                        <textarea type="" class="form-control" id="detalle2" placeholder="" ></textarea>
-                    </div>
+                    <div class="form-row">
+                        <div class="col">
+                            <label for="exampleInputEmail1">Detalle de lo sucedido</label>
+                            <textarea type="text" class="form-control" id="detalle1" placeholder="" ></textarea>
+                        </div>
+                        <div class="col">
+                            <label for="exampleInputEmail1">Que solicita </label>
+                            <textarea type="" class="form-control" id="detalle2" placeholder="" ></textarea>
+                        </div>
 
-                </div>
-                  <div class="form-row">
-                 
-                    <div class="form-group">
-                        <label for="exampleFormControlFile1">Seleccione un Pdf de la factura</label>
-                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
                     </div>
-                </div>
-         </div>
-           <br>
-          
-                <button type="submit" class="btn btn-secondary">Submit</button>
-            </form>
-             {{vista}}
+                    <div class="form-row">
+                    
+                        <div class="form-group">
+                            <label for="exampleFormControlFile1">Seleccione un Pdf de la factura</label>
+                            <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                        </div>
+                    </div>
+            </div>
+            <br>
+            
+            <button type="button"  class="btn btn-secondary" v-on:click="alta_queja">Submit</button>        
+    </form>
+    
+         {{vista}}    
     </div>
 </template>
 <script>
 import axios from "axios";
 export default {
-  created() {
-    axios.get("https://jsonplaceholder.typicode.com/todos/1").then((result) => {
-      console.log(result.data);
-    })
-  },
-  data(){
+//   created() {
+//     axios.get("http://localhost/Quejas_api/cliente.php?id=42").then((result) => {
+//       console.log(result.data);
+//     })
+//   },
+  
+  data:function(){
       return{
+         opcionseleccionada:2,
+         form:{
+             "opcion2": "",
+             "departamento":"",
+             "municipio":"",
+             "estado":"recibido",
+             "nombre":"",
+             "cui":"",
+             "telefono":"",
+             "celular":"",
+             "correo":"",
+             "direccion":"",
+             "empresa":"",
+             "nit":"",
+             "direccionemp":"",
+             "zona":"",
+             "telefonoemp":"",
+             "correoemp":""
+         },
          vista:'noanonimo'
       }
   },
   mounted(){
-      this.opcionver()
-      
+       this.opcionver()
+       
   },
+  
   methods: {
       opcionver(){
           var vistaver = this.$route.params.opcion;
-          console.log(vistaver)
-          this.vista=vistaver
+        //   console.log(vistaver)
+          this.vista=vistaver;
+          if(vistaver=='anonimo'){
+               this.opcionseleccionada=1;
+                console.log(this.opcionseleccionada)
+                , console.log(this.form);
+          }
+      },
+      alta_queja(){
+          console.log(this.form);
+           axios.post("http://localhost/Quejas_api/cliente.php",this.form)
+           .then(data => {
+                 console.log(data);
+           })
+        
       }
     // cambio2(){
     //     var valor= this.vista
