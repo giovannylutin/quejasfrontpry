51fbdaa57c18b8665108faded3f4b83323145509
giovanny2 <agiovannylutin@gmail.com>
2021-09-09 22:53:38 -0600

    reportes formulario

--- src/vistas/interno_reportes.vue
+++ src/vistas/interno_reportes.vue
@@ -2,13 +2,20 @@
     <div class="Reportes">
         <div class="Reportes_datos">
              <div class="Reportes_datos-titulo">
-                <h4>Empresa</h4>
+                <h5>Empresa</h5>
             </div>
             <div class="Reportes_datos-campos">
                 <div class="form-row">
                     <div class="col">
                     <label for="exampleInputEmail1">Nit Empresa:</label>
-                    <input type="text" class="form-control form-control-sm" id="direccion" placeholder="Nit Empresa" >
+                    <!-- <select name="empresas" id=""></select> -->
+                    <!-- <input type="text" v-model="nemp" v-on:keypress="busquedaemp" class="form-control form-control-sm" id="direccion" placeholder="Nit Empresa" > -->
+                    
+                    <select class="form-control form-control-sm" v-model="nemp" v-on:change="mostrarcampos" >
+                            <option value="" disabled selected hidden>Seleccione Region</option>
+                            <option v-for="(emptotal) in empresast" v-bind:key ="emptotal.NIT" :value="emptotal.NIT">{{emptotal.EMPRESA}}</option>
+                    </select>
+                        
                     </div>
                 </div>
                 <div class="form-row">
@@ -16,12 +23,17 @@
                     <label for="exampleInputEmail1">Fecha:</label>
                     <input type="date" class="form-control form-control-sm" id="direccion" placeholder="Nit Empresa" >
                     </div>
+                    <div class="col">
+                    <label for="exampleInputEmail1">Fecha:</label>
+                    <input type="date" class="form-control form-control-sm" id="direccion" placeholder="Nit Empresa" >
+                    </div>
+                    
                 </div>
-                <div class="dropdown-divider"></div>
+                
             </div>
-
+<div class="dropdown-divider"></div>
             <div class="Reportes_datos-titulo">
-                <h4>Ubicacion</h4>
+                <h5>Ubicacion</h5>
             </div>
             <div class="Reportes_datos-campos">
 
@@ -67,49 +79,53 @@
                 <h4>datos</h4>
             </div>
             <div class="Reportes_graficas-informacion">
+               <!-- <div>
+                    <h6>Fecha Hasta: </h6>
+                    Pollo
+                </div> -->
                 <div>
-                    empresa
+                    <h6>Nit Empresa:</h6>
+                    {{this.nemp}}
                 </div>
                 <div>
-                    total quejas pais
+                    <h6>Quejas Recibidas:</h6>
+                    {{totalquejas}}
+                    
                 </div>   
                 <div>
-                    mes en curso quejas
+                    <h6>Fecha Desde: </h6>
+                    2021-01-01
                 </div>
+                <div>
+                    <h6>Fecha Hasta: </h6>
+                    2021-09-09
+                </div>
+                <!-- <div>
+                    <h6>Fecha Hasta: </h6>
+                    2021-09-09
+                </div> -->
             </div>
             <div class="Reportes_graficas-detalles">
                 
                 <div>
-                <table class="table table-striped">
+                    <table class="table table-striped table-sm">
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">region</th>
+      <th scope="col">departamento</th>
+      <th scope="col">empresa</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
+    <tr v-for="(resempresas) in datosrep" v-bind:key ="resempresas.legth">
       <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
+      <td>{{resempresas.REGION}}</td>
+      <td>{{resempresas.DEPARTAMENTO}}</td>
+      <td>{{resempresas.EMPRESA}}</td>
     </tr>
   </tbody>
-</table>
+                    </table>
                 </div>
             
             </div>
@@ -125,11 +141,16 @@ export default({
             dep:[],
             mun:[],
             idreg:"",
-            iddep:""
+            iddep:"",
+            nemp:"",
+            empresast:[],
+            datosrep:[],
+            totalquejas:""
         }
     },
     mounted(){
-        this.regionver()
+        this.regionver(),
+        this.busquedaemp()
     },
     methods: {
         regionver(){
@@ -147,11 +168,29 @@ export default({
       })
         },
         municipiover(){
-                   axios.get ("http://localhost/Quejas_api/reportes.php?id="+this.iddep+"&verop=2") .then (res => {// Aquí está la escritura de ES6. La dirección de la solicitud de obtención es el archivo php que el propio editor almacena en el sitio web. Introduce su escritura, también puedes definir la tuya
+                 axios.get ("http://localhost/Quejas_api/reportes.php?id="+this.iddep+"&verop=2") .then (res => {// Aquí está la escritura de ES6. La dirección de la solicitud de obtención es el archivo php que el propio editor almacena en el sitio web. Introduce su escritura, también puedes definir la tuya
                  this.mun = res.data; // Obtener datos
-                 console.log(this.mun);
+                //  console.log(this.mun);
+                 })
+        },
+        busquedaemp(){
+            axios.get ("http://localhost/Quejas_api/reportes.php?id="+0+"&verop=3") .then (res => {// Aquí está la escritura de ES6. La dirección de la solicitud de obtención es el archivo php que el propio editor almacena en el sitio web. Introduce su escritura, también puedes definir la tuya
+                 this.empresast = res.data; // Obtener datos
+                //  console.log(this.empresast);
+                 })
+        },
+        mostrarcampos(){
+             axios.get ("http://localhost/Quejas_api/reportes.php?id="+this.idreg+"&verop=4") .then (res => {
+                 this.datosrep = res.data; // Obtener datos
+                //  console.log(this.empresast);
+                 console.log(this.nemp)
+                 this.totalquejas= this.datosrep.length
+                 
+                 })
+            //SELECT * FROM `tb_region` JOIN `tb_departamento`on `tb_region`.`ID_REGION`=`tb_departamento`.`ID_REGION`join `tb_quejas`on `tb_departamento`.`ID_DEP`=`tb_quejas`.`ID_DEP` JOIN `tb_quejas_proveedor`on `tb_quejas`.`ID_QUEJA`=`tb_quejas_proveedor`.`ID_QUEJA`JOIN `tb_quejas_empresas`on `tb_quejas_proveedor`.`NIT`= `tb_quejas_empresas`.`NIT` WHERE (`tb_quejas`.`FECHA_ALTA`) BETWEEN '2021-09-01' AND '2021-09-20'
            
-      })
+            //SELECT REGION,DEPARTAMENTO,EMPRESA,DIRECCION FROM `tb_region` JOIN `tb_departamento`on `tb_region`.`ID_REGION`=`tb_departamento`.`ID_REGION`join `tb_quejas`on `tb_departamento`.`ID_DEP`=`tb_quejas`.`ID_DEP` JOIN `tb_quejas_proveedor`on `tb_quejas`.`ID_QUEJA`=`tb_quejas_proveedor`.`ID_QUEJA`JOIN `tb_quejas_empresas`on `tb_quejas_proveedor`.`NIT`= `tb_quejas_empresas`.`NIT` WHERE (`tb_quejas`.`FECHA_ALTA`) BETWEEN '2021-09-01' AND '2021-09-20'
+ 
         }
     }
 })
@@ -164,6 +203,9 @@ export default({
     height: 100%;
     display: flex;
 }
+.Reportes label{
+    margin-bottom: 0;
+}
 .Reportes_datos{
     /* outline: 2px solid blue; */
     width: 25%;
@@ -171,7 +213,7 @@ export default({
     background-color: lightgray;
     border-radius: 10px;
 }
-.Reportes_datos-titulo h4{
+.Reportes_datos-titulo h5,h4{
     text-align: center;
     padding-top: 3px;
     margin-bottom: 0;
@@ -191,12 +233,23 @@ button{
     margin-left: 8px;
 }
 .Reportes_graficas-informacion{
-    /* outline: 2px solid red; */
-    height: 25%;
+    outline: 2px solid red;
+    height: 17%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.Reportes_graficas-informacion div{
+    margin: 8px;
+    width: 150px;
+    text-align: center;
+    border-radius: 10px;
+    padding: 3px;
+    border: 3px solid grey;
 }
 .Reportes_graficas-detalles{
     /* outline: 2px solid red; */
-    height: 65%;
+    height: 40%;
      overflow-y: scroll;
 }
 
