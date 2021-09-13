e4300f291b9ca486ff2005358cc944431a3d06ef
giovanny2 <agiovannylutin@gmail.com>
2021-09-12 19:33:54 -0600

    fase uno panel principal ok

--- src/vistas/interno_dash.vue
+++ src/vistas/interno_dash.vue
@@ -1,20 +1,147 @@
 <template>
     <div class="paneldash">
-        <div class="paneldash_detalle"></div>
+   
+    <div class="paneldash_nav">
+         <button type="button" class="btn btn-warning btn-sm" v-on:click="salir">Salir</button>
+    </div>
+    <div class="paneldash_info">
+        <div class="paneldash_detalle">
+
+            <div class="paneldash_detalle-infodiarias">
+                <img src="http://drive.google.com/uc?export=view&id=1G3zSoCYRkq-36Zs15KhvnWV0O7FGl0tz" width="60px" height="60px" alt="">
+                 <div>
+                    <h5>Quejas Ingresadas Hoy</h5>
+                    <h3>{{this.resumen.diatotal}}</h3>
+                </div>
+            </div>
+            <div class="paneldash_detalle-infodiaantes">
+                <img src="http://drive.google.com/uc?export=view&id=1G3zSoCYRkq-36Zs15KhvnWV0O7FGl0tz" width="60px" height="60px" alt="">
+                 <div>
+                    <h5>Quejas Ingresadas Ayer</h5>
+                    <h3>{{this.resumen.diaantes}}</h3>
+                </div>
+            </div>
+             <div class="paneldash_detalle-infomes">
+                <img src="http://drive.google.com/uc?export=view&id=1aaqg5YRmkasCezdCooP49qSNEeL95mO8" width="60px" height="60px" alt="">
+                 <div>
+                    <h5>Quejas de este mes</h5>
+                    <h3>{{this.resumen.mestotal}}</h3>
+                </div>
+            </div>
+             <div class="paneldash_detalle-infomes">
+                <img src="http://drive.google.com/uc?export=view&id=1aaqg5YRmkasCezdCooP49qSNEeL95mO8" width="60px" height="60px" alt="">
+                 <div>
+                    <h5>Quejas Durante el año</h5>
+                    <h3>{{this.resumen.anual}}</h3>
+                </div>
+            </div>
+
+        </div>
         <div class="paneldash_grafica"></div>
     </div>
+    </div>
 </template>
+<script>
+import axios from "axios";
+
+export default ({
+    
+    data() {
+        return{
+            estado:"",
+            resumen:[]
+        }
+    },
+    mounted(){
+
+        if(localStorage.getItem('tk_sesion')){
+            // console.log("hola")
+            this.cargaresumen();
+        }else{
+            this.$router.push('/Ingreso')
+        }
+    },
+    methods:{
+        salir(){
+            localStorage.removeItem('tk_sesion')
+            this.$router.push('/Ingreso')
+        },
+        cargaresumen(){
+            axios.get ("http://localhost/Quejas_api/ingreso.php") .then (res => { 
+                this.resumen = res.data; 
+                console.log(this.resumen)
+            })
+        }
+    }
+})
+</script>
+
 <style scoped>
 .paneldash{
     /* outline: 2px solid red; */
     width: 100%;
     height: 100%;
     display: flex;
+    flex-direction: column;
+}
+.paneldash_nav{
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    /* outline: 2px solid blueviolet; */
+}
+.paneldash_info{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
 }
 .paneldash_detalle{
-    outline: 2px solid green;
+    /* outline: 2px solid green; */
     width: 30%;
     height: 100%;
+    display: flex;
+    flex-direction: column;
+    /* justify-content: center; */
+    /* padding: 5px; */
+}
+.paneldash_detalle-infodiarias{
+    /* outline:2px solid blueviolet; */
+    height:20%;
+    width: 100%;
+    background: #12ba38;
+    border-radius: 15px;
+    color: white;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    margin-bottom: 15px;
+}
+.paneldash_detalle-infomes{
+    /* outline:2px solid blueviolet; */
+    height:20%;
+    width: 100%;
+    background: orange;
+    border-radius: 15px;
+    color: white;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    padding-left: 7px ;
+    margin-bottom: 15px;
+}
+.paneldash_detalle-infodiaantes{
+    height:20%;
+    width: 100%;
+    background: tan;
+    border-radius: 15px;
+    color: white;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    padding-left: 7px ;
+    margin-bottom: 15px;
 }
 .paneldash_grafica{
     outline: 2px solid red;
--- src/vistas/interno_login.vue
+++ src/vistas/interno_login.vue
@@ -8,20 +8,20 @@
         <div class="row">
             <label for="staticEmail" class="col-sm-3">Correo:</label>
             <div class="col-lg">
-            <input type="Email" class="form-control" v-model="usuariop" required>
+            <input type="Email" class="form-control" v-model="campos.usuariop" required>
             </div>
         </div>
         <div class="row">
             <label for="inputPassword" class="col-sm-3 ">Contraseña:</label>
             <div class="col-lg">
-            <input type="password" class="form-control" v-model="passp">
+            <input type="password" class="form-control" v-model="campos.passp">
             </div>
         </div>
     </div>
     
     <div class="login_botones">
         
-        <button type="button" class="btn btn-success" v-on:click="ingresar">Ingresar</button>
+        <button type="button" class="btn btn-success" v-on:click="autenticar" id="inputPassword">Ingresar</button>
     </div>
 </div>
 </template>
@@ -31,11 +31,37 @@ export default ({
     
     data() {
         return{
-            usuariop:"",
-            passp:""
+            // usuariop:"",
+            // passp:"",
+            campos:{
+                usuariop:"",
+                passp:""
+            }
         }
     },
       methods:{
+          autenticar(){
+            //   console.log(Global.url)
+            // console.log(this.form);
+            axios.post("http://localhost/Quejas_api/ingreso.php",this.campos)
+            .then(data => {
+                  if(data.status==200){ 
+                      if(data.request.statusText!="Usuario no encontrado"){
+                        
+                            // console.log()
+                            localStorage.setItem('tk_sesion',data.data[0].TOK)
+                            // localStorage.setItem()
+                            this.$router.push('/Principal')
+                        // console.log(this.campos)
+                      }else{
+                          console.log("opps")
+                      }        
+                    // var valortk = data.statusText;
+                    //   alert("se inserto correctamente"+valortk);
+                    // this.$router.push('/'+valortk +'/Consulta')
+                  }
+           })
+          },
           ingresar(){
               if(this.usuariop=="" || this.passp==""){
                   alert("al parecer hay campos vacios")
