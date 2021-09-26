1b0ac4cc7e813561586d9b3547ec3aea79c8a35b
giovanny2 <agiovannylutin@gmail.com>
2021-09-26 01:48:54 -0600

    mend

--- src/vistas/interno_login.vue
+++ src/vistas/interno_login.vue
@@ -61,12 +61,13 @@ export default ({
                     if(data.request.statusText!="Usuario no encontrado"){
                         
                              // console.log()
-                             localStorage.setItem('tk_sesion',data.data[0].TOK)
-                            // localStorage.setItem('tk_sesion',"45asd4a5sd5a")
-                            //  console.log(data.data[0].TOK)
-                              this.$router.push('/Principal')
+                            //  localStorage.setItem('tk_sesion',data.data[0].TOK)
+                            localStorage.setItem('tk_sesion',"45asd4a5sd5a")
+                            //  console.log(data.request)
+                           this.$router.push('/Principal')
                         // console.log(this.campos)
                     }else{
+                        console.log(data.request)
                         this.flashMessage.show({status:'warning',title:'!!OOPSS',message:'Alparecer tu USUARIO Y CONTRASEÑA SON INCORRECTOS ',time: 5000});
                     }
              }
